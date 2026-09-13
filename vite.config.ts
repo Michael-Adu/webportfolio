533fda5d94a7c4fcfc969ee91cb113bab9c5759e
Michael Nimdea-Gyan Adu <72309685+Michael-Adu@users.noreply.github.com>
2026-09-13 23:25:04 +0100

    Revert package lock changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/michael-adu-embedded-software-engineer/",
+  base: "/webportfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.md', '**/*.pdf', '**/*.glb', '**/*.mp4']
 })
